4a561456acba38505f27d10498896e5a97d635e2
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2022-11-06 11:35:50 -0500

    Feature: default grid of divs

--- script.js
+++ script.js
@@ -7,20 +7,44 @@
 // have a clear button just to reset every square to white
 
 
+//(600/innerSquareWidth)^2 = total number of squares 
+
+
 let gridSize;
-let numberOfSquares;
-// const outerSquare = document.querySelector('#outerSquare');
-// const innerSquares = document.querySelector('#innerSquares');
+let innerSquareWidth;
+// const outerSquare = document.getElementById('outerSquare');
+// const innerSquares = document.createElement('div');
+//  innerSquares.innerHTML = "working";
+//  outerSquare.appendChild(innerSquares);
+
+
+
+
+// for(let i = 0; i < 16; i++){
+//     for(let j = 0; j < 16; j++){
+//         let innerSquares = document.createElement('div');
+//         innerSquares.innerHTML = `<div class="innerSquares"></div>`;
+//         outerSquare.appendChild(innerSquares);
+//     }
+// }
 
 document.getElementById('submit').onclick = () => {
     gridSize = document.getElementById('gridSize').value;
     if(gridSize > 60 || gridSize < 1){
         window.alert('Please input a number between 1 and 60.')
         gridSize = 30;
+        return;
     }
     console.log(gridSize);
-    numberOfSquares = 600/gridSize;
-    console.log(`each square will be ${numberOfSquares} pixels wide`);
+    innerSquareWidth = 600/gridSize;
+    console.log(`each square will be ${innerSquareWidth} pixels wide`);
+    console.log(Math.pow((600/innerSquareWidth), 2));
+
+    for(let i=0; i < Math.pow((600/innerSquareWidth), 2); i++){
+        let innerSquares = document.createElement('div');
+        innerSquares.innerHTML = `<div class="innerSquares">woking</div>`;
+        outerSquare.appendChild(innerSquares);
+    }
 }
 
 
